59059bd36857fe23f98fb56b374cb4da82f94928
Astakhov Sergey <fzastahov@gmail.com>
2021-11-07 14:47:28 +0300

    converter

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 #include <string>
 #include <list>
 #include <iomanip>
+#include <map>
 
 using namespace std;
 
-struct Candidate {
-    enum Fraction {
-        FRACTION_EDRO,
-        FRACTION_YABLOKO,
-        FRACTION_LDPR,
-        FRACTION_NOVYE_LUDI,
-        FRACTION_KPRF
-    };
+enum Fraction
+{
+    FRACTION_EDRO,
+    FRACTION_YABLOKO,
+    FRACTION_LDPR,
+    FRACTION_NOVYE_LUDI,
+    FRACTION_KPRF
+};
 
+struct Candidate
+{
     string fio;
     uint age;
     float income;
@@ -21,25 +24,40 @@ struct Candidate {
     uint voices;
 };
 
-int main(){
-    list<Candidate> candidates_list
+class Converter
+{
+private:
+    static inline map<Fraction, string> fraction_to_string = {{FRACTION_EDRO, "EDRO"},
+                                                              {FRACTION_YABLOKO, "YABLOKO"},
+                                                              {FRACTION_LDPR, "LDPR"},
+                                                              {FRACTION_NOVYE_LUDI, "NOVIE LUDI"},
+                                                              {FRACTION_KPRF, "KPRF"}};
+
+public:
+    static const string &toString(Fraction fraction_code)
     {
-        Candidate{"Zhirinovsky V.V.", 75, 2000000.156, 
-            Candidate::FRACTION_LDPR, 42345},
-        Candidate{"Kaz M.E", 36, 1000000.3, 
-            Candidate::FRACTION_YABLOKO, 12043},
+        return fraction_to_string.find(fraction_code)->second;
+    }
+};
+
+int main()
+{
+    list<Candidate> candidates_list{
+        Candidate{"Zhirinovsky V.V.", 75, 2000000.156,
+                  FRACTION_LDPR, 42345},
+        Candidate{"Kaz M.E", 36, 1000000.3,
+                  FRACTION_YABLOKO, 12043},
         Candidate{"Zyganov G.A", 77, 1500000.3452,
-            Candidate::FRACTION_KPRF, 24021}
-    };
+                  FRACTION_KPRF, 24021}};
 
-
-
-    for(const auto o: candidates_list){
-        cout << o.fio 
-        << "\nage: " << o.age 
-        << "\nincome: " << setprecision(9) << o.income 
-        << "\nfraction: " << o.fraction 
-        << "\nvoices:" << o.voices << endl << endl;
+    for (const auto o : candidates_list)
+    {
+        cout << o.fio
+             << "\nage: " << o.age
+             << "\nincome: " << setprecision(9) << o.income
+             << "\nfraction: " << Converter::toString(o.fraction)
+             << "\nvoices:" << o.voices << endl
+             << endl;
     }
 
     return 0;
